34ffa94d8d94b8c1f3d2491e411da4f578835571
Shrujan Kharwadey <kharwadeyshrujan@gmail.com>
2025-05-05 23:35:50 +0530

    updated postpack file

--- packages/evershop/postpack.js
+++ packages/evershop/postpack.js
@@ -1,30 +1,38 @@
 const fs = require('fs');
 const path = require('path');
 
-// Get the current version of the package from the nearest package.json file
-const packageJson = require(path.resolve(__dirname, 'package.json'));
-const {version} = packageJson;
-// Get the --pack-destination from the command line arguments
-// Create a package.json file in the packDestination directory with dependencies is the package itself
-fs.writeFileSync(
-  path.resolve(process.env.npm_config_pack_destination, 'package.json'),
-  JSON.stringify(
-    {
-      name: packageJson.name,
-      version,
-      dependencies: {
-        '@evershop/evershop': `file:./evershop-evershop-${version}.tgz`
-      },
-      scripts: {
-        setup: 'evershop install',
-        start: 'evershop start',
-        'start:debug': 'evershop start:debug',
-        build: 'evershop build',
-        dev: 'evershop dev',
-        'user:create': 'evershop user:create'
-      }
+try {
+  // Get the current version of the package from the nearest package.json file
+  const packageJson = require(path.resolve(__dirname, 'package.json'));
+  const { version } = packageJson;
+
+  // Validate the pack destination environment variable
+  const packDestination = process.env.npm_config_pack_destination;
+  if (!packDestination) {
+    throw new Error('The environment variable "npm_config_pack_destination" is not defined.');
+  }
+
+  // Create a package.json file in the packDestination directory with dependencies on the package itself
+  const packageData = {
+    name: packageJson.name,
+    version,
+    dependencies: {
+      '@evershop/evershop': `file:./evershop-evershop-${version}.tgz`
     },
-    null,
-    2
-  )
-);
+    scripts: {
+      setup: 'evershop install',
+      start: 'evershop start',
+      'start:debug': 'evershop start:debug',
+      build: 'evershop build',
+      dev: 'evershop dev',
+      'user:create': 'evershop user:create'
+    }
+  };
+
+  const packageJsonPath = path.resolve(packDestination, 'package.json');
+  fs.writeFileSync(packageJsonPath, JSON.stringify(packageData, null, 2));
+
+  console.log(`Successfully created package.json at ${packageJsonPath}`);
+} catch (error) {
+  console.error('Error during postpack script execution:', error.message);
+}
