9a6f98eb78f4ef1eaf4b4202c4d3ff01453cc668
Shrujan Kharwadey <kharwadeyshrujan@gmail.com>
2025-06-13 20:05:31 +0530

    Tweak ESLint settings

--- jest.config.js
+++ jest.config.js
@@ -15,3 +15,4 @@ const config = {
 module.exports = config;
 // Update dependencies at 2024-10-17 09:44:06
 // Improve documentation at 2024-11-05 17:41:43
+// Tweak ESLint settings at 2024-11-25 16:02:57
